e4a6d8f272bb1640218a5419eb64c1b8a2828944
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-05-07 18:48:38 +0200

    Start implementing Question.play method

--- quiz/Question.py
+++ quiz/Question.py
@@ -1,4 +1,5 @@
 import json
+from random import randint
 
 
 class Question:
@@ -9,6 +10,34 @@ class Question:
         self.question = question
         self.answers = answers
         self.good_answer = good_answer
+        self.used = False
+
+    def __str__(self) -> str:
+        return f'{self.question}'
+
+    def play(self):
+        print('\n', end='')
+        print("="*50)
+
+        print(self)
+        for number, answer in enumerate(self.answers, start=1):
+            print(f'{number}. {answer}')
+        
+        print('\n', end='')
+        user_answer = int(input('Your answer: '))
+        print(self.answers[user_answer])
+
+    @ staticmethod
+    def draw():
+        unused_questions = [q for q in Question.questions if q.used == False]
+        number_of_unused_questions = len(unused_questions)
+        if number_of_unused_questions > 0:
+            draw_question = unused_questions[randint(0, number_of_unused_questions - 1)]
+            draw_question.used = True
+            return draw_question
+        else:
+            return None
+
 
     @ staticmethod
     def load_questions():
--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,5 +1,19 @@
 from Question import Question
 
 Question.load_questions()
-for question in Question.questions:
-    print(f'{question.question} | {question.answers} | {question.good_answer}')
+
+print("Welcome in Quiz game")
+while True:
+    question = Question.draw()
+    if question:
+        print('\n', end='')
+        print("="*50)
+
+        print(question)
+        for number, answer in enumerate(question.answers, start=1):
+            print(f'{number}. {answer}')
+        
+        print('\n', end='')
+        user_answer = input('Your answer: ')
+    else:
+        break
